8933c8e5e6964db764e0aae73717e59108352207
StjWorld <st_murat@hotmail.com>
2023-08-08 10:04:04 -0500

    rebase test

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 
 export default () => {
     return (<div>
-            <h1>THIS IS NEW NAV BAR</h1>
+            <h1>TEST NAV BAR</h1>
             <hr />
             <MarketingApp />
         </div>
